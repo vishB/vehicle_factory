3f37e264052805d11b10c018e0c160d9b7a4c83c
Vishal Bharti <vishB>
2014-01-13 15:49:26 +0530

    Added numeric only validation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -91,10 +91,29 @@ $(document).ready(function() {
     $(".up").hide();
     $(".down").show();
   });
+
   $(".down").click(function(){
     $("#user_vehicles").slideDown();
     $(".down").hide();
     $(".up").show();
   });
+  
+  $(".numeric_only").keydown(function(event) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ( $.inArray(event.keyCode,[46,8,9,27,13,190]) !== -1 ||
+             // Allow: Ctrl+A
+            (event.keyCode == 65 && event.ctrlKey === true) || 
+             // Allow: home, end, left, right
+            (event.keyCode >= 35 && event.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        else {
+            // Ensure that it is a number and stop the keypress
+            if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                event.preventDefault(); 
+            }   
+        }
+    });
 });
 
