1b2fdb0f7d05873f45c02af9bf10e313672430fd
Vishal Bharti <vishB>
2014-01-14 14:15:46 +0530

    for testing

--- app/models/construction.rb
+++ app/models/construction.rb
@@ -1,5 +1,4 @@
 class Construction < ActiveRecord::Base
   attr_accessible :delivery_date, :start_date, :user_id, :vehicle_id,:engine_id,:v_identifier
   belongs_to :vehicles
-  validate :delivery_date, :start_date, :presence_of => true, :if => vehicle.construction.present?
 end
