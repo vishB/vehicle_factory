25f20bfa7f25df597eb77a3db0f13ed8bfcd961e
Vishal Bharti <vishB>
2014-01-14 14:00:46 +0530

    Added model validations to construction dates

--- app/models/construction.rb
+++ app/models/construction.rb
@@ -1,4 +1,5 @@
 class Construction < ActiveRecord::Base
   attr_accessible :delivery_date, :start_date, :user_id, :vehicle_id,:engine_id,:v_identifier
   belongs_to :vehicles
+  validate :delivery_date, :start_date, :presence_of => true, :if => vehicle.construction.present?
 end
