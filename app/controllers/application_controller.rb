cdc0da09f9dda5d19d247349d4ced944801c3951
Vishal Bharti <vishB>
2014-01-19 21:06:20 +0530

    made UI changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,8 @@ class ApplicationController < ActionController::Base
     all_users = User.all
     @available_users = all_users - vehicle_users
   end
-
+  
+  # Check if logged in user is admin
   def check_admin
     unless current_user.admin?
       redirect_to root_path
